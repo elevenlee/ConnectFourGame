1d633d0683dcba2371be40e44043f7b4070a73e7
shenli <sl3268@nyu.edu>
2014-11-30 00:00:25 -0800

    Add game container and factory for UI implementation

---
+++ src/edu/nyu/cs/connectfour/container/factory/GameContainerFactory.java
@@ -0,0 +1,68 @@
+package edu.nyu.cs.connectfour.container.factory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.swing.JFrame;
+
+import edu.nyu.cs.connectfour.container.GetContainerable;
+import edu.nyu.cs.connectfour.container.impl.GameContainer;
+import edu.nyu.cs.connectfour.utils.ParameterChecker;
+
+/**
+ * @author shenli
+ * <p>
+ * Factory object that can vend main component for the game.
+ * <p>
+ * NOTE: The factory object is thread-safe.
+ */
+public class GameContainerFactory {
+    private static Set<String> idSet = new CopyOnWriteArraySet<>();
+    private static List<GetContainerable<JFrame>> containers = new CopyOnWriteArrayList<>();
+    
+    /**
+     * Suppress default constructor for non-instantiable
+     */
+    private GameContainerFactory() {
+        throw new AssertionError();
+    }
+    
+    /**
+     * Create a main component for the game based on the specific id, component width and height.
+     * <p>
+     * @param id the identifier of the component
+     * @param width the width of this component in pixels
+     * @param height the height of this component in pixels
+     * @return a suitable game component
+     */
+    public static GetContainerable<JFrame> getGameContainer(String id, int width, int height) {
+        ParameterChecker.nullCheck(id, "game container id");
+        ParameterChecker.emptyCheck(id, "game container id");
+        ParameterChecker.rangeCheck(width, "game container width");
+        ParameterChecker.rangeCheck(height, "game container height");
+        if (!idSet.add(id)) {
+            throw new IllegalArgumentException("GameContainer id: " + id + " already taken!");
+        }
+        
+        GetContainerable<JFrame> container = new GameContainer(id, width, height);
+        containers.add(container);
+        return container;
+    }
+    
+    /**
+     * Returns an unmodifiable view of the {@link edu.nyu.cs.connectfour.container.GetContainerable<JFrame>} list. 
+     * This method allows modules to provide users with "read-only" access to internal lists. Query operations 
+     * on the returned list "read through" to the specified list, and attempts to modify the returned list, 
+     * whether direct or via its iterator, result in an {@code UnsupportedOperationException}.
+     * <p>
+     * @return an unmodifiable view of the specified list. If no {@link edu.nyu.cs.connectfour.container.GetContainerable<JFrame>}
+     * have been created, returns an empty list
+     */
+    public static List<GetContainerable<JFrame>> getGames() {
+        return Collections.unmodifiableList(containers);
+    }
+    
+}
---
+++ src/edu/nyu/cs/connectfour/container/impl/GameContainer.java
@@ -0,0 +1,76 @@
+package edu.nyu.cs.connectfour.container.impl;
+
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import edu.nyu.cs.connectfour.container.GetContainerable;
+import edu.nyu.cs.connectfour.ui.factory.PanelFactory;
+import edu.nyu.cs.connectfour.ui.menu.GameMenuBar;
+import edu.nyu.cs.connectfour.ui.type.PanelType;
+
+/**
+ * @author shenli
+ * <p>
+ * A {@code GameContainer} object is used to create an "Game" main window.
+ * <p>
+ * The {@code GameContainer} object contains a {@link javax.swing.JFrame} component. {@code GameContainer} 
+ * object may be obtains by calls on FrameFactory.getGame factory methods. These will return the {@code GameContainer} object.
+ * <p>
+ * <b>Warning:</b> Swing is not thread safe.
+ */
+public class GameContainer implements GetContainerable<JFrame> {
+    private static final String ICON_PATH = "res/symbol.png";
+    private static final String TITLE = "Connect Four";
+    
+    private final JFrame frame;
+    private final String id;
+    
+    /**
+     * Creates an "Game" main frame with the specific identifier, width and height.
+     * <p>
+     * @param id the identify of this game container object
+     * @param width the width of this component in pixels
+     * @param height the height of this component in pixels
+     */
+    public GameContainer(String id, int width, int height) {
+        assert id != null && !id.equals("");
+        assert width > 0;
+        assert height > 0;
+        
+        this.id = id;
+        this.frame = new JFrame();
+        this.frame.setTitle(TITLE + " @ " + id);
+        this.frame.setIconImage(new ImageIcon(ICON_PATH, "Game Symbol").getImage());
+        this.frame.setJMenuBar(GameMenuBar.INSTANCE.getContainer());
+        this.frame.setLayout(new BorderLayout());
+        this.frame.add(BorderLayout.NORTH, PanelFactory.getPanel(PanelType.PLAYER).getContainer());
+        this.frame.add(BorderLayout.SOUTH, PanelFactory.getPanel(PanelType.CONTROL).getContainer());
+        this.frame.add(BorderLayout.WEST, PanelFactory.getPanel(PanelType.EMPTY).getContainer());
+        this.frame.add(BorderLayout.EAST, PanelFactory.getPanel(PanelType.UTILITY).getContainer());
+        this.frame.add(BorderLayout.CENTER, PanelFactory.getPanel(PanelType.CHESS).getContainer());
+        
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(width, height);
+        this.frame.setResizable(false);
+        this.frame.setLocationRelativeTo(null);
+    }
+    
+    /**
+     * Return string representation of this game container identifier.
+     * @return string representation of this game container identifier
+     */
+    public String getID() {
+        return id;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JFrame getContainer() {
+        return frame;
+    }
+
+}
