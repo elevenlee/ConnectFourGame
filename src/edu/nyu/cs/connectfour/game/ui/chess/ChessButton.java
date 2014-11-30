9609f305e1f4e3922808ff5b501ccd9f4ac92983
shenli <sl3268@nyu.edu>
2014-11-29 21:44:27 -0800

    Add chess buttons implementation and factory methods

---
+++ src/edu/nyu/cs/connectfour/game/ui/chess/ChessButton.java
@@ -0,0 +1,65 @@
+package edu.nyu.cs.connectfour.game.ui.chess;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JButton;
+
+/**
+ * @author shenli
+ * <p>
+ * An implementation of a "push" chess button.
+ * <p>
+ * Chess Buttons can be configured, and to some degree controlled, by Actions. Using an Action with a button 
+ * has many benefits beyond directly configuring a button. {@code ChessButton} object may be obtains by calls 
+ * on {@link edu.nyu.cs.connectfour.game.ui.factory.ChessButtonFactory#getChessButton()} factory methods. 
+ * These will create a chess button.
+ * <p>
+ * <b>Warning:</b> Swing is not thread safe.
+ */
+public class ChessButton extends JButton {
+    private static final long serialVersionUID = -5229743049810920053L;
+    
+    private Shape shape;
+    
+    /**
+     * Creates a button with no set text or icon.
+     */
+    public ChessButton() {
+        super();
+        Dimension size = this.getPreferredSize();
+        size.width = size.height = Math.max(size.width, size.height);
+        this.setPreferredSize(size);
+        this.setContentAreaFilled(false);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isArmed()) {
+            g.setColor(Color.LIGHT_GRAY);
+        } else {
+            g.setColor(this.getBackground());
+        }
+        g.fillOval(2, 2, this.getSize().width - 4, this.getSize().height - 4);
+        super.paintComponents(g);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean contains(int x, int y) {
+        if ((shape == null)
+                || (!shape.getBounds().equals(this.getBounds()))) {
+            shape = new Ellipse2D.Float(0, 0, this.getWidth(), this.getHeight());
+        }
+        return shape.contains(x, y);
+    }
+    
+}
---
+++ src/edu/nyu/cs/connectfour/game/ui/factory/ChessButtonFactory.java
@@ -0,0 +1,52 @@
+package edu.nyu.cs.connectfour.game.ui.factory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.swing.JButton;
+
+import edu.nyu.cs.connectfour.game.ui.chess.ChessButton;
+
+/**
+ * @author shenli
+ * <p>
+ * Factory object that can vend {@link javax.swing.JButton} for the game.
+ * <p>
+ * NOTE: The factory object is thread-safe.
+ */
+public class ChessButtonFactory {
+    private static final List<JButton> buttons = new CopyOnWriteArrayList<>();
+    
+    /**
+     * Suppress default constructor for non-instantiable
+     */
+    private ChessButtonFactory() {
+        throw new AssertionError();
+    }
+    
+    /**
+     * Create a {@link javax.swing.JButton} object for the game.
+     * <p>
+     * @return a suitable {@link javax.swing.JButton}
+     */
+    public static JButton getChessButton() {
+        JButton cb = new ChessButton();
+        buttons.add(cb);
+        return cb;
+    }
+    
+    /**
+     * Returns an unmodifiable view of the {@link javax.swing.JButton} list. This method allows modules to 
+     * provide users with "read-only" access to internal lists. Query operations on the returned list "read 
+     * through" to the specified list, and attempts to modify the returned list, whether direct or via its 
+     * iterator, result in an {@code UnsupportedOperationException}.
+     * <p>
+     * @return an unmodifiable view of the specified list. If no {@link javax.swing.JButton} have been created, 
+     * returns an empty list
+     */
+    public static List<JButton> getChessButtons() {
+        return Collections.unmodifiableList(buttons);
+    }
+    
+}
